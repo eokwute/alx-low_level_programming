b9d76cffa6fe41fa1a2951be5e0defead42968f9
eokwute <okwute.emmanuel@yahoo.com>
2023-08-03 15:07:54 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -32,12 +32,3 @@ if (i * i == n)
 return (i);
 return (actual_sqrt_recursion(n, i + 1));
 }
-if (i * i > n)
-
-return (-1);
-if (i * i == n)
-
-return (i);
-return (actual_sqrt_recursion(n, i + 1));
-}
-
